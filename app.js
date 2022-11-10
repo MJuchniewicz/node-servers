5ceacbcfb348a711fcd507c988d75d14a5ba229a
Mikael_Gienson <juchniewicz.m@gmail.com>
2022-11-10 14:46:42 +0100

    Done with basic logic

--- app.js
+++ app.js
@@ -2,26 +2,29 @@ const express = require('express');
 
 const app = express();
 
+//register view engine
+app.set('view engine', 'ejs')
+
 app.listen(3000);
 
-
-
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', {root: __dirname});
-    // res.send('<p>Home Page<p/>')
+    const blogs = [
+        {title: 'kdjhaskjhdajsd', snippet: 'kdjahskjdhajkshdjhasd'},
+        {title: 'hdfghdfgh', snippet: 'kdjahskjhhfghdfghdfghdfghdfghdfghdhajkshdjhasd'},
+        {title: 'hhfg', snippet: 'kdjahskjdhajkhhghghghghghghghhshdjhasd'},
+    ]
+    res.render('index', { title: 'Home', blogs});
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile("./views/about.html", { root: __dirname });
-// res.send('<p>Home Page<p/>')
+    res.render("about", { title: "About" });
 });
 
-app.get('/about-us', (req, res) => {
-    res.redirect('/about')
-})
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create a new blog" });
+});
 
 // use this funcion for all incoming requests
 app.use((req, res) => {
-    res.sendFile("./views/404.html", { root: __dirname });
-    res.statusCode = 404
+    res.status(404).render("404", { title: "404" });
 })
