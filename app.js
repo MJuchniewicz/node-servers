7076a96857c192cb032bd01948bc471027bc15de
Mikael_Gienson <juchniewicz.m@gmail.com>
2022-11-11 10:16:47 +0100

    Added routes, GET and POST requests and details view

--- app.js
+++ app.js
@@ -1,24 +1,75 @@
-const express = require('express');
+const express = require("express");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoose = require("mongoose");
+const Blog = require("./models/blog");
+const { BADFLAGS } = require("dns");
 
 const app = express();
 
+// connect to mongodb & listen for requests
+const dbURI = process.env.MONGO_API_KEY;
+
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .then(() => console.log("connectedo to db"))
+  .catch((err) => console.log(err));
+
 //register view engine
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
-app.listen(3000);
-
-app.get('/', (req, res) => {
-    const blogs = [
-        {title: 'kdjhaskjhdajsd', snippet: 'kdjahskjdhajkshdjhasd'},
-        {title: 'hdfghdfgh', snippet: 'kdjahskjhhfghdfghdfghdfghdfghdfghdhajkshdjhasd'},
-        {title: 'hhfg', snippet: 'kdjahskjdhajkhhghghghghghghghhshdjhasd'},
-    ]
-    res.render('index', { title: 'Home', blogs});
+//middleware & static files
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+app.use(morgan("dev"));
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
 });
 
-app.get('/about', (req, res) => {
-    res.render("about", { title: "About" });
+app.get("/", (req, res) => {
+  res.redirect("/blogs");
 });
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});
+
+// blog routes
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("index", { title: "All Blogs", blogs: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post('/blogs', (req, res) => {
+  const blog = new Blog(req.body)
+
+  blog.save()
+    .then((result) => {
+    res.redirect('/blogs')
+    }).catch((err) => {
+    console.log(err)
+  })
+})
+
+app.get('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  
+  Blog.findById(id)
+    .then(result => {
+    res.render('details', { blog: result, title: "Blog details"})
+    }).catch((err) => {
+    console.log(err)
+  })
+})
 
 app.get("/blogs/create", (req, res) => {
   res.render("create", { title: "Create a new blog" });
@@ -26,5 +77,5 @@ app.get("/blogs/create", (req, res) => {
 
 // use this funcion for all incoming requests
 app.use((req, res) => {
-    res.status(404).render("404", { title: "404" });
-})
+  res.status(404).render("404", { title: "404" });
+});
